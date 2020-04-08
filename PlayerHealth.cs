6a6f0982eac0d51225a042ce2ce68a811eca5d83
Bah2 <57127295+Bah2@users.noreply.github.com>
2020-04-08 15:10:11 +0300

    fiksattu vesikuolema

--- PlayerHealth.cs
+++ PlayerHealth.cs
@@ -6,11 +6,12 @@ using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public float regenSpeed=2;
+    public float regenSpeed = 2;
     public float startingHealth = 100;                            // The amount of health the player starts the game with.
     public float currentHealth;                                   // The current health the player has.
     public Slider healthSlider;                                 // Reference to the UI's health bar.
     public Image damageImage;                                   // Reference to an image to flash on the screen on being hurt.
+    public AudioClip damageClip;
     public AudioClip deathClip;                                 // The audio clip to play when the player dies.
     public float flashSpeed = 5f;                               // The speed the damageImage will fade at.
     public Color flashColour = new Color(1f, 0f, 0f, 0.1f);     // The colour the damageImage is set to, to flash.
@@ -18,16 +19,20 @@ public class PlayerHealth : MonoBehaviour
     public int wait;
 
     Animator anim;                                              // Reference to the Animator component.
+    AudioSource damageAudio;
     AudioSource playerAudio;                                    // Reference to the AudioSource component.
     Player_Move playerMovement;                                 // Reference to the player's movement.
     public bool isDead;                                                // Whether the player is dead.
     public bool damaged;                                               // True when the player gets damaged.
     public bool safe;
 
+    public Texture2D picture;
+
     void Awake()
     {
         // Setting up the references.
-//        anim = GetComponent<Animator>();
+        //        anim = GetComponent<Animator>();
+        damageAudio = GetComponent<AudioSource>();
         playerAudio = GetComponent<AudioSource>();
         playerMovement = GetComponent<Player_Move>();
 
@@ -38,9 +43,12 @@ public class PlayerHealth : MonoBehaviour
 
     void Update()
     {
-        if (safe && currentHealth<startingHealth && !isDead)
-           currentHealth += regenSpeed * Time.deltaTime;
-           
+        if (isDead) //purkkaviritys, et ei regenaa vedessä
+            safe = false;
+
+        if (safe && currentHealth < startingHealth && !isDead)
+            currentHealth += regenSpeed * Time.deltaTime;
+
 
         // If the player has just been damaged...
 
@@ -74,9 +82,8 @@ public class PlayerHealth : MonoBehaviour
 
 
         // Play the hurt sound effect.
-        //        playerAudio.Play();
-        //playerAudio.clip = deathClip;
-        //playerAudio.Play();
+        damageAudio.clip = damageClip;
+        damageAudio.Play();
 
 
         // If the player has lost all it's health and the death flag hasn't been set yet...
@@ -91,9 +98,9 @@ public class PlayerHealth : MonoBehaviour
     {
         if (collision.collider.name == "WaterCollider")
         {
-            TakeDamage(100);
+            TakeDamage(200);
         }
-        
+
     }
 
 
@@ -101,10 +108,11 @@ public class PlayerHealth : MonoBehaviour
     {
         // Set the death flag so this function won't be called again.
         isDead = true;
+        safe = false;
 
         Debug.Log("kual");
         // Tell the animator that the player is dead.
-//        anim.SetTrigger("Die");
+        //        anim.SetTrigger("Die");
 
         // Set the audiosource to play the death clip and play it (this will stop the hurt sound from playing).
         playerAudio.clip = deathClip;
@@ -113,11 +121,21 @@ public class PlayerHealth : MonoBehaviour
         // Turn off the movement and shooting scripts.
         playerMovement.enabled = false;
 
-        StartCoroutine(Wait(1.5f));
-        
+        StartCoroutine(Wait(3f));
+
+        Cursor.visible = (true);
+
     }
 
-    IEnumerator Wait(float sec)
+    void OnGUI()
+    {
+        if (isDead == true)
+        {
+            GUI.DrawTexture(new Rect(500, 100, 900, 700), picture);
+        }
+    }
+
+    IEnumerator Wait(float sec) //venaillaan parametrin määräämän ajan ennen menua
     {
         yield return new WaitForSeconds(sec);
         SceneManager.LoadScene("start_menu", LoadSceneMode.Single);
